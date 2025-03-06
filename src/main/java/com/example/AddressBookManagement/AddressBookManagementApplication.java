35d3937e32d77aa2123690a225aeda1ecc60a594
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-03-06 12:32:28 +0530

    Address Book Management Project Setup

---
+++ src/main/java/com/example/AddressBookManagement/AddressBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.AddressBookManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookManagementApplication.class, args);
+	}
+
+}
